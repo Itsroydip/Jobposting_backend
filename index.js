ceec12214448c6860939a33e044e08a9e04056ed
Itsroydip <roydiptarka204@gmail.com>
2024-12-28 17:08:18 +0530

    added: user route signup/signin

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const env = require('dotenv');
 env.config();
+const mongoose = require('mongoose');
+const MONGO_URI = process.env.MONGO_URI;
+const userRoutes = require('./routes/user');
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use("/api/user", userRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Hello World!!!")
@@ -10,4 +18,14 @@ app.get("/",(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log("Listening at port 3000....")
+    mongoose.connect(MONGO_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>{
+        console.log("Connected to MongoDB.......")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 })
---
+++ routes/user.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../schemas/user.schema");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const env = require('dotenv');
+env.config()
+
+// sign up ---> Input user credentials ----> look for existing user ----> If user exists ----> Tell them email
+// already exists ----> If not ----> Create new user in Mongodb -----> send back jwt token
+
+router.post("/signup", async(req,res)=>{
+    try{
+        const {email,password,name,phone} = req.body;
+        const isUserExist = await User.findOne({email});
+        console.log(isUserExist)
+
+        if(isUserExist){
+            res.status(400).json({message: "Email already exists"});
+            return
+        }
+        else{
+            const hashedPassword = bcrypt.hashSync(password,10);
+            const newUser = await new User({email, password: hashedPassword, name, phone}).save();
+            const token = jwt.sign({email},process.env.JWT_SECRET);
+            return res.status(200).json({message: "User created successfully", token, id:newUser._id})
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({message: "Server error"})
+    }
+})
+
+router.post("/signin", async(req,res)=>{
+    try {
+        const {email, password} = req.body;
+        const isUserExist = await User.findOne({email});
+
+        if(!isUserExist){
+            res.status(400).json({message: "Invalid email or password!"})
+            return;
+        }
+        else{
+            const isPasswordCorrect = bcrypt.compare(password, isUserExist.password);
+
+            if(!isPasswordCorrect){
+                res.status(400).json({message:"Invalid email or password"})
+                return;
+            }
+            const token = jwt.sign({email},process.env.JWT_SECRET);
+            res.status(200).json({message:"Login successfull", token, id:isUserExist._id})
+
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Server error"})
+    }
+})
+
+module.exports = router; 
